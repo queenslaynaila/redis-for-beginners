fa8144c5dfdcf023762f3d0f960b60da676094ff
Queenslay Jema <queenslayjema@gmail.com>
2024-08-05 18:54:12 +0300

    chore: Add Redis dependency, create Redis client, and display books on the home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,18 @@
 import Link from 'next/link'
+import { client } from './lib/db'
 
 const getBooks = async () => {
-  
+  const result = await client.zRangeWithScores('books', 0, -1)
+ const books = Promise.all(result.map((b)=>{
+  return client.hGetAll(`book:${b.score}`)
+ }))
+  return books
 }
 
 export default async function Home() {
+
+  const books = await getBooks()
+
 
   return (
     <main>
@@ -12,7 +20,14 @@ export default async function Home() {
         <h1 className='font-bold'>Books on Redis!</h1>
         <Link href="/create" className="btn">Add a new book</Link>
       </nav>
-      
+      {books.map((book) => (
+        <div key={book.title} className='card'>
+          <h2>{book.title}</h2>
+          <p>{book.author}</p>
+          <p>{book.blurb}</p>
+          <p>{book.rating}</p>
+        </div>
+      ))}
       <p>List of books here.</p>
     </main>
   )
