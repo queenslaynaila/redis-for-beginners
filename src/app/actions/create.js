40c4c50210d1277dd12d60218b709e0929f734a5
Queenslay Jema <queenslayjema@gmail.com>
2024-08-05 18:39:52 +0300

    chore: Add Redis dependency and create Redis client

--- src/app/actions/create.js
+++ src/app/actions/create.js
@@ -1,10 +1,23 @@
 'use server'
 
-// import { client } from "@/lib/db"
+import { client } from '../lib/db'
 import { redirect } from 'next/navigation'
 
 export async function createBook(formData) {
   const {title, rating, author, blurb} = Object.fromEntries(formData)
-
-  
+  const id = Math.floor(Math.random() * 100000)
+  const unique = await client.zAdd('books',{
+    value:title,
+    score:id
+  }, { NX: true})
+  if(!unique){
+    return {error:"the book eciss"}
+  }
+  await client.hSet(`book:${id}`, {
+    title,
+    rating,
+    author,
+    blurb
+  })
+  redirect('/')
 }
